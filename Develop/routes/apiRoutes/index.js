99e74de3fc2358a8fd24761b6081bb2886b22251
ienglehart <ienglehart13@gmail.com>
2021-02-05 17:03:26 -0500

    Some stuff
    this is from a couple weeks ago when i worked on this last and doesnt look like a ton

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -12,11 +12,8 @@ router.get('/notes', (req, res) => {
 // post api/notes 
 router.post('/notes', (req, res) => {
     //set id, how do i send id to database?
-    req.body.id = notes.length.toString();
-
-    let newNote = res.body;
-    notes.push(newNote);
-    return notes;
+    console.log(req.body);
+    res.json(req.body);
 });
 
 //delete api/notes
