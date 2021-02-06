e051bdae901aabc213c41a79f7d505b0000d71de
ienglehart <ienglehart13@gmail.com>
2021-02-05 20:20:09 -0500

    Funcional finally
    actually used fs, and it works now wooo

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,26 +1,34 @@
+const fs = require('fs');
 const router = require('express').Router();
 
 //maybe not this?
-const { notes } = require('../../db/db.json')
+//const { notes } = require('../../db/db.json');
+const { createNewNote, filterByQuery } = require('../../lib/notes');
+
+fs.readFile("db/db.json", (err, data) => {
+
+    if (err) throw err;
+    var notes = JSON.parse(data);
+
 
 // get api/notes
 router.get('/notes', (req, res) => {
-    let results = notes;
-    if(req.query){
-        results = filterByQuery(req.query, results)
-    }
-    res.json(results);
+    res.json(notes);
 });
 
 // post api/notes 
 router.post('/notes', (req, res) => {
     //set id, how do i send id to database?
-    console.log(req.body);
-    res.json(req.body);
+    let newNote = req.body;
+    notes.push(newNote);
+    
+
+    const note = createNewNote(req.body, notes)
+    res.json(note)
 });
 
 //delete api/notes
-
+});
 
 
 module.exports = router;
